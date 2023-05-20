455d9fff1328cdd7488a7bc2c2adb1bd5ee013b9
Nick <49327729+tpbnick@users.noreply.github.com>
2023-05-19 20:40:08 -0400

    Removed jQuery from script.js Updated images.

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,53 @@
-$(document).ready(function(){
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
-        } else {
-            $('#back-to-top').fadeOut();
-        }
-    });
-    // scroll body to 0px on click
-    $('#back-to-top').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500);
-        return false;
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll', function () {
+    const backToTopButton = document.querySelector('#back-to-top');
+    if (window.pageYOffset > 50) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+
+  document.querySelector('#back-to-top').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+  window.checkDivs = function () {
+    const portfolioImages = document.getElementById('portfolio-images');
+    const imageCarousel = document.getElementById('image-carousel');
+
+    if (getComputedStyle(portfolioImages).display !== 'none') {
+      location.href = '#portfolio-images';
+    }
+
+    if (getComputedStyle(imageCarousel).display !== 'none') {
+      location.href = '#image-carousel';
+    }
+  };
+
+  const body = document.getElementsByTagName('body')[0];
+  let resizeObserver = new ResizeObserver(() => {
+    if (window.innerWidth < 1200) {
+      document.querySelector('.big-portfolio').style.display = 'none';
+      document.querySelector('.image-carousel').style.display = 'block';
+    } else {
+      document.querySelector('.big-portfolio').style.display = 'block';
+      document.querySelector('.image-carousel').style.display = 'none';
+    }
+  });
+  resizeObserver.observe(body);
+
+  function setupVideos() {
+    for (const video of document.querySelectorAll('video')) {
+      video.controls = false;
+      video.addEventListener('mouseover', () => {
+        video.controls = true;
+      });
+      video.addEventListener('mouseout', () => {
+        video.controls = false;
+      });
+    }
+  }
+  setupVideos();
 });
